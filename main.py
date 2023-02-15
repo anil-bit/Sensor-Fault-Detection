3d40aaa81a47863cf14a0b57ad25e9c54d4520f8
Anil <53553840+anil-bit@users.noreply.github.com>
2023-02-15 16:12:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sys
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
 
-
 from sensor.pipeline.training_pipeline import TrainPipeline
 
 if __name__ == '__main__':
