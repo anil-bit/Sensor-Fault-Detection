d2b5e76ac35eeab0707d90116d4d679576eed9f4
Anil <53553840+anil-bit@users.noreply.github.com>
2023-02-15 15:17:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from sensor.constant.training_pipeline import SCHEMA_FILE_PATH
 
 import yaml
 
-
 def read_yaml_file(file_path: str) -> dict:
         with open(file_path,"rb") as yaml_file:
             return yaml.safe_load(yaml_file)
